8b53d9e64b630b76295d6728602a015396e308f6
intmain84 <aheleha@gmail.com>
2023-12-25 13:16:49 +0600

    sdfsdf

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+//Routes
+app.get("/hello", (req, res) => {
+  res.send("Task manager");
+});
+
+//Server
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening ${port}`));
