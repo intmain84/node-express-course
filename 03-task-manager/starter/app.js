f81ee19bb48602ea0fdb5f314ebd0e9f34994ba7
intmain84 <aheleha@gmail.com>
2023-12-26 02:51:11 +0600

    sfdsf

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 
+const tasks = require("./routes/tasks");
+
+//Middlewares
+app.use(express.json());
+
 //Routes
 app.get("/hello", (req, res) => {
   res.send("Task manager");
 });
+
+app.use("/api/v1/tasks", tasks);
 
 //Server
 const port = 3000;
---
+++ 03-task-manager/starter/controllers/tasks.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const getAllTasks = (req, res) => {
+  res.send("All tasks");
+};
+
+const createTask = (req, res) => {
+  res.send("Create task");
+};
+
+const getTask = (req, res) => {
+  res.send("Get task");
+};
+
+const updateTask = (req, res) => {
+  res.send("Update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("Delete task");
+};
+
+module.exports = {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+};
