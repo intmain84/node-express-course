2439d805d256fd604dd2abc3281e55e7b3a33c83
intmain84 <aheleha@gmail.com>
2023-12-31 18:29:13 +0600

    hjg

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -2,16 +2,42 @@ const Task = require("../models/Task");
 const express = require("express");
 
 const getAllTasks = async (req, res) => {
-  res.send("All tasks");
+  try {
+    const allTasks = await Task.find({});
+    res.status(200).json({
+      status: "Success",
+      data: allTasks,
+    });
+  } catch (error) {
+    res.status(404).json({
+      msg: error,
+    });
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(200).json(task);
+  try {
+    const task = await Task.create(req.body);
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({
+      msg: error,
+    });
+  }
 };
 
-const getTask = (req, res) => {
-  res.send("Get task");
+const getTask = async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id).exec();
+    res.status(200).json({
+      status: "Success",
+      data: task,
+    });
+  } catch (error) {
+    res.status(404).json({
+      msg: error,
+    });
+  }
 };
 
 const updateTask = (req, res) => {
