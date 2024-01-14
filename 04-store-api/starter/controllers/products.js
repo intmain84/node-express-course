1ebddb63127e7cb0c6e5fa08efcff234195564c4
intmain84 <aheleha@gmail.com>
2024-01-15 00:18:41 +0600

    rtwere

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,13 +1,44 @@
+const Product = require("../models/product");
+
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({
-    msg: "Products testing route",
-  });
+  const search = "al";
+  const products = await Product.find({}).select("name price").limit(4);
+  res.status(200).json({ products, nmHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({
-    msg: "Products route",
-  });
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObj = {};
+
+  if (featured) {
+    queryObj.featured = featured === "true" ? true : false;
+  }
+
+  if (company) {
+    queryObj.company = company;
+  }
+
+  if (name) {
+    queryObj.name = { $regex: name, $options: "i" };
+  }
+  let result = Product.find(queryObj);
+
+  //Sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  //Select
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
+  res.status(200).json(products);
 };
 
 module.exports = {
