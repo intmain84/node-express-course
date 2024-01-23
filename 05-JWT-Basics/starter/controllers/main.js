74252cf056a355f4d2ce934821bf3b7b81d52254
intmain84 <aheleha@gmail.com>
2024-01-24 00:13:59 +0600

    sdfsdfs

---
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -0,0 +1,23 @@
+const CustomAPIError = require("../errors/custom-error");
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError("Provide username or password", 400);
+  }
+
+  res.send("Login/Register/Signup Route");
+};
+
+const dashboard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, John Doe`,
+    secret: `Here is your authorized data, your lucky number is: ${luckyNumber}`,
+  });
+};
+
+module.exports = {
+  login,
+  dashboard,
+};
