b8b83a539d9927c031ccf3a12664f66cd96e293c
intmain84 <aheleha@gmail.com>
2024-01-25 23:03:25 +0600

    dd

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,4 +1,5 @@
 const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
   const { username, password } = req.body;
@@ -6,10 +7,27 @@ const login = async (req, res) => {
     throw new CustomAPIError("Provide username or password", 400);
   }
 
-  res.send("Login/Register/Signup Route");
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "User created", token });
 };
 
 const dashboard = async (req, res) => {
+  //Вытаскиваем Bearer с токеном
+  const authHeader = req.headers.authorization;
+
+  //Проверяем есть ли оно вообще + пришло ли оно в формате Bearer 00000
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+  console.log(token);
+
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
     msg: `Hello, John Doe`,
