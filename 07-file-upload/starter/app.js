589ea4a4a9de716de7a45e074289a785b761809a
intmain84 <aheleha@gmail.com>
2024-03-18 10:35:55 +0500

    PostmanUpload

--- 07-file-upload/starter/app.js
+++ 07-file-upload/starter/app.js
@@ -21,7 +21,6 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/v1/products', productRouter);
-app.use('/api/v1/products', uploadProductImages);
 
 // middleware
 app.use(notFoundMiddleware);
--- 07-file-upload/starter/controllers/productController.js
+++ 07-file-upload/starter/controllers/productController.js
@@ -2,9 +2,9 @@ const Product = require('../models/Product');
 const {StatusCodes} = require('http-status-codes')
 
 const createProduct = async (req, res) => {
-//   const product = await Product.create(req.body);
-//   res.status(StatusCodes.CREATED).json({ product });
-res.send('created product')
+    console.log(req.body);
+    const product = await Product.create(req.body);
+    res.status(StatusCodes.CREATED).json(product)
 };
 
 const getAllProducts = async (req, res) => {
