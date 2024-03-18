5469f94308245cae54494a6c8afcc01da0ffc147
intmain84 <aheleha@gmail.com>
2024-03-18 11:45:56 +0500

    UploadsFolder

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,8 +1,14 @@
 const {StatusCodes} = require('http-status-codes')
+const path = require('path')
 
 const uploadProductImage = async (req, res) => {
-    console.log(req);
-    res.send('upload product image')
+    const productImage = req.files.image;
+
+    const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`);
+
+    await productImage.mv(imagePath);
+
+    return res.status(StatusCodes.OK).json({image: {src:`/uploads/${productImage.name}`}})
 }
 
 module.exports = {
